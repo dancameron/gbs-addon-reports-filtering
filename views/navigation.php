d8c0c3a93d05ecc5c59df6ed6da67a1a44b3a3e6
Dan Cameron <dan@sproutventure.com>
2012-11-21 10:27:57 -0800

    Desc.

--- views/navigation.php
+++ views/navigation.php
@@ -1,2 +1,3 @@
 <input name="filter" id="filter-box" value="" maxlength="30" size="30" type="text" placeholder="<?php gb_e('Filter') ?>" class="text-input">
 <input id="filter-clear-button" type="submit" value="<?php gb_e('Clear') ?>" class="alt_button"/>
+<span id="ff_desc"><?php gb_e('Search the entire report, sort columns then download the CSV of the filtered report.') ?></span>
